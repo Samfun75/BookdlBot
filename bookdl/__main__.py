cc1830319673c8bab65408893b379e309c1e8328
Samfun75 <38332931+Samfun75@users.noreply.github.com>
2022-04-27 13:57:02 +0300

    Replace deprecated get_event_loop() method

--- bookdl/__main__.py
+++ bookdl/__main__.py
@@ -4,14 +4,13 @@ from pyrogram import idle
 from bookdl.telegram import BookDLBot
 
 logger = logging.getLogger(__name__)
+
+
 async def main():
     await BookDLBot.start()
     await idle()
 
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    try:
-        loop.run_until_complete(main())
-    except KeyboardInterrupt:
-        logger.error("KeyboardInterruption: Services Terminated!")
+    BookDLBot.run(main())
+    logger.info("Services Terminated!")
